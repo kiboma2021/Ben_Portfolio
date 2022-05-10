d7c1788ac093864f10e58f2ec8a2a79edc3b17e7
Ben Kiboma <omayioben@gmail.com>
2022-05-10 18:13:36 +0300

    Add header

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
-function hideUnhide() {
-  var x=document.getElementById
-}
+const hamBug=document.querySelector(".hamburger");
+const navMenu=document.querySelector(".nav_menu");
+
+hamBug.addEventListener("click", () => {
+  hamBug.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav_link").forEach(n => n.
+  addEventListener("click", () => {
+    hamBug.classList.remove("active");
+    navMenu.classList.remove("active");
+  }))
