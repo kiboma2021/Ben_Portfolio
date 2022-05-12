72b1ec088e34d1536a530347c69d6528f9ec6e46
Ben Kiboma <omayioben@gmail.com>
2022-05-12 18:24:01 +0300

    Style Popup window

--- javascript.js
+++ javascript.js
@@ -15,3 +15,46 @@ document.querySelectorAll(".nav_link").forEach((n) =>
     body.classList.remove("no-scroll");
   })
 );
+
+
+/* Modal forms */
+
+const openModalButtons=document.querySelectorAll('[data-modal-target]')
+const closeModalButtons=document.querySelectorAll('[data-close-button]')
+const overlay=document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+  const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+})
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+  const modal = button.closest('.modal')
+    closeModal(modal)
+})
+})
+
+function openModal(modal) {
+  if (modal==null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+  body.classList.toggle("no-scroll");
+  
+}
+
+function closeModal(modal) {
+  if (modal==null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+  body.classList.remove("no-scroll");
+}
